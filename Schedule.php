41c9d02288017802eaa7b0841cda61880521e552
wgraber <kal.zekdor@gmail.com>
2019-02-23 02:02:59 +0000

    [Utility.Scheduler] * checkTrigger method implemented. * fromCron is now a static factory method. (Returns a Schedule object.)

--- Schedule.php
+++ Schedule.php
@@ -7,6 +7,9 @@ class Schedule
 	protected $Rules;
 	protected $Items;
 	protected $Intersections;
+	protected $TemporalPrecedence;
+	protected $LastTrigger;
+	protected $NextTrigger;
 	
 	/**
 	 * @param array $aRules An array of Rule objects defining the schedule.
@@ -37,6 +40,8 @@ class Schedule
 			$this->TemporalPrecedence = $aTemporalPrecedence;
 		}
 		
+		$this->LastTrigger = new \DateTime();
+		$this->NextTrigger = null;
 	}
 	
 	/**
@@ -55,6 +60,8 @@ class Schedule
 			$this->Rules[$sRuleName] = $oRule;	
 			$this->sortRules();
 		}
+		
+		$this->NextTrigger = null; //Reset stored next trigger, as we've changed our schedule.
 	}
 	
 	/**
@@ -144,9 +151,96 @@ class Schedule
 		}
 	}
 	
+	/**
+	 * Checks if a given DateTime (defaults to the current time) is after the next scheduled occurrence.
+	 * Keeps track of last and next triggers. Only needs to recalculate next trigger when a trigger is met, so this method should be performant enough to call repeatedly.
+	 * Last trigger is initially set to the time the Schedule object was created, and will thus trigger on the first occurrence after that time.
+	 * Use setLastTrigger() to override the setting if necessary.
+	 * @param \DateTime $dDateTime DateTime to check trigger for. Will return true if the given DateTime is after (or exactly equal to) the next scheduled ocurrence. Defaults to the current time.
+	 * @return bool True if triggered, False otherwise.
+	 */
 	public function checkTrigger(\DateTime $dDateTime = null) : bool
 	{
-		throw new Exception\NotImplementedException("Trigger checking not implemented yet.");
+		if ($this->NextTrigger === null)
+		{
+			$this->NextTrigger = $this->getNextOccurrence($this->LastTrigger);
+		}
+		
+		if ($dDateTime === null)
+		{
+			$dDateTime = new \DateTime();
+		}
+		
+		if ($dDateTime >= $this->NextTrigger)
+		{
+			$this->LastTrigger = Temporal\Second::incrementDate($this->NextTrigger); //Need to increment so we don't keep getting the same trigger.
+			$this->NextTrigger = $this->getNextOccurrence($this->LastTrigger);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	/**
+	* Sets the last triggered time to the specified DateTime, and recalculate Next Trigger accordingly.
+	* @param \DateTime $dDateTime DateTime to set as last triggered time.
+	*/
+	public function setLastTrigger(\DateTime $dDateTime)
+	{
+		$this->LastTrigger = $dDateTime;
+		$this->NextTrigger = $this->getNextOccurrence($this->LastTrigger);
+	}
+	
+	/**
+	* Static method to create a schedule based on a cron-like string. Currently only supports Minutes/Hour, Hour/Day, Day/Month, and Month/Year.
+	* @param string $sCronString A cron-like string to create a schedule from.
+	* @return Schedule
+	*/
+	static function fromCron($sCronString)
+	{
+		$oSchedule = new Schedule();
+		
+		$cronArr = explode(' ', $sCronString);
+
+		$cronPairs = [[new Temporal\Minute, new Temporal\Hour, 0],
+						[new Temporal\Hour, new Temporal\Day, 0],
+						[new Temporal\Day, new Temporal\Month, -1],
+						[new Temporal\Month, new Temporal\Year, -1]];
+						
+
+		foreach ($cronArr as $index => $val)
+		{
+			
+			$rule = new Rule($cronPairs[$index][0], $cronPairs[$index][1]);
+			if ($val != '*' && stripos($val, '/') === false)
+			{
+				$rule->createSet(self::cronRangeToSet($val, $cronPairs[$index][2]));
+			}
+			else
+			{
+				if (stripos($val, ',') !== false)
+				{
+					throw new Exception\NotImplementedException("Cron strings do not currently support mixing intervals and sets.");
+				}
+				
+				if (stripos($val, '/') !== false)
+				{
+					$recurrenceArr = explode("/", $val);
+					
+					$rule->Interval = (int) $recurrenceArr[1];
+				
+					if (stripos($recurrenceArr[0], '-') !== false)
+					{
+						$rangeArr = explode('-', $recurrenceArr[0]);
+						$rule->Start = (int) $rangeArr[0] + $cronPairs[$index][2];
+						$rule->End = (int) $rangeArr[1] + $cronPairs[$index][2];
+					}
+				}
+			}
+			$oSchedule->addRule($rule);
+		}
+		
+		return $oSchedule;
 	}
 	
 	protected function getIntersection(int $iItem) : array
@@ -289,7 +383,7 @@ class Schedule
 		}
 	}
 	
-	protected function cronRangeToSet($s, $mod)
+	protected static function cronRangeToSet($s, $mod)
 	{
 		if (stripos($s, '/') !== false)
 		{
@@ -322,43 +416,5 @@ class Schedule
 		return $aSet;
 	}
 	
-	public function fromCron($sCronString)
-	{
-		$cronArr = explode(' ', $sCronString);
-
-		$cronPairs = [[new Temporal\Minute, new Temporal\Hour, 0],
-						[new Temporal\Hour, new Temporal\Day, 0],
-						[new Temporal\Day, new Temporal\Month, -1],
-						[new Temporal\Month, new Temporal\Year, -1]];
-						
-
-		foreach ($cronArr as $index => $val)
-		{
-			
-			$rule = new Rule($cronPairs[$index][0], $cronPairs[$index][1]);
-			if ($val != '*' && stripos($val, '/') === false)
-			{
-				$rule->createSet($this->cronRangeToSet($val, $cronPairs[$index][2]));
-			}
-			else
-			{
-				if (stripos($val, ',') !== false)
-				{
-					throw new Exception\NotImplementedException("Cron strings do not currently support mixing intervals and sets.");
-				}
-				
-				$recurrenceArr = explode("/", $val);
-				
-				$rule->Interval = (int) $recurrenceArr[1];
-				
-				if (stripos($recurrenceArr[0], '-') !== false)
-				{
-					$rangeArr = explode('-', $recurrenceArr[0]);
-					$rule->Start = (int) $rangeArr[0] + $cronPairs[$index][2];
-					$rule->End = (int) $rangeArr[1] + $cronPairs[$index][2];
-				}
-			}
-			$this->addRule($rule);
-		}
-	}
+	
 }
